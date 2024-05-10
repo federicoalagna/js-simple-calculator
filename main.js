c31dae74c13e66c48d364d503a911577fa2bc57f
federicoalagna <federicoalagna01@gmail.com>
2024-05-10 17:04:04 +0200

    pulsanti funzionali

--- main.js
+++ main.js
@@ -4,10 +4,16 @@
 function addNumber(numero) {
     let result = document.getElementById("result");
     result.textContent += numero;
-    console.log(result)
+    console.log("Numero aggiunto:", numero);
 }
 
 //--aggiungo la funzione a ogni pulsante per far si che vengano mostrati sulla calcolatrice
-document.getElementById("pulsante-1").addEventListener("click", function() {
-    addNumber("1");
-});
+for (let i = 1; i <= 9; i++) {
+    document.getElementById("pulsante-" + i).addEventListener("click", function() {
+        addNumber(i.toString());
+        console.log("Pulsante", i, "click");
+        console.log("Risultato attuale:", result.textContent); 
+    });
+  
+
+}
